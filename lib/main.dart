bf5828b2fe5298948b13fbec04f65913ff349939
new-well <takahiro.arai.sb@gmail.com>
2022-08-06 18:15:14 +0900

    [update] 履歴表示と、削除機能

--- lib/main.dart
+++ lib/main.dart
@@ -24,27 +24,44 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
   final String title;
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  int _addNum = 0;
-
   final TextEditingController _textEditingController = TextEditingController();
 
-  void _addCounter() {
+  int _counter = 0;
+  int _addNum = 0;
+  final List<int> _history = [];
+
+  void _refreshCounter() {
     setState(() {
-      _counter += _addNum;
+      if (_addNum != 0) {
+        _history.add(_addNum);
+        _addNum = 0;
+      }
+      _counter = _history.isNotEmpty
+          ? _history.reduce((value, element) => value + element)
+          : 0;
       _textEditingController.clear();
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    final focusNode = FocusNode();
+    final FocusNode focusNode = FocusNode();
+
+    final Size size = MediaQuery.of(context).size;
+    final EdgeInsets padding = MediaQuery.of(context).padding;
+    double maxHeight = size.height - padding.top - padding.bottom - 50;
+
+    // 計算結果エリアの高さ
+    final double resultAreaHeight = maxHeight * (30 / 100);
+    // 履歴表示エリアの高さ
+    final double historyAreaHeight = maxHeight * (70 / 100);
+    // テキスト履歴の文字エリアの高さ
+    const double historyHeaderAreaHeight = 30;
 
     return Focus(
       focusNode: focusNode,
@@ -54,39 +71,80 @@ class _MyHomePageState extends State<MyHomePage> {
           appBar: AppBar(
             title: Text(widget.title),
           ),
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                const Text(
-                  '現在の利用金額',
-                ),
-                Text(
-                  '$_counter',
-                  style: Theme.of(context).textTheme.headline4,
-                ),
-                Container(
-                  padding: const EdgeInsets.only(right: 20, left: 20),
-                  child: TextField(
-                    controller: _textEditingController,
-                    keyboardType: TextInputType.number,
-                    inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: '金額を入力',
+          body: Column(
+            children: <Widget>[
+              Container(
+                width: size.width,
+                height: resultAreaHeight,
+                padding: const EdgeInsets.only(right: 20, left: 20),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    const Text('現在の利用金額'),
+                    Text(
+                      '$_counter',
+                      style: Theme.of(context).textTheme.headline4,
                     ),
-                    onChanged: (value) {
-                      setState(() {
-                        _addNum = value == "" ? 0 : int.parse(value);
-                      });
-                    },
-                  ),
+                    TextField(
+                      controller: _textEditingController,
+                      keyboardType: TextInputType.number,
+                      inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: '金額を入力',
+                      ),
+                      onChanged: (value) {
+                        setState(() {
+                          _addNum = value == "" ? 0 : int.parse(value);
+                        });
+                      },
+                    ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+              Container(
+                width: size.width,
+                height: historyAreaHeight,
+                padding: const EdgeInsets.only(right: 20, left: 20),
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: historyHeaderAreaHeight,
+                      width: size.width,
+                      child: const Text('履歴'),
+                    ),
+                    SizedBox(
+                      height: historyAreaHeight - historyHeaderAreaHeight,
+                      width: size.width,
+                      child: Scrollbar(
+                        child: ListView.builder(
+                          itemCount: _history.length,
+                          itemBuilder: (context, index) {
+                            return Dismissible(
+                              key: UniqueKey(),
+                              child: Card(
+                                child: ListTile(
+                                  title: Text('${_history[index]}'),
+                                ),
+                              ),
+                              onDismissed: (direction) {
+                                setState(() {
+                                  _history.removeAt(index);
+                                });
+                                _refreshCounter();
+                              },
+                            );
+                          },
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
           floatingActionButton: FloatingActionButton(
-            onPressed: _addCounter,
+            onPressed: _refreshCounter,
             tooltip: 'Increment',
             child: const Icon(Icons.add),
           ),
